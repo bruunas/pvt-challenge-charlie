248ae140bd3fe4fa917e04216dae6fb6926ff730
Bruna Santos <brunas.dev@gmail.com>
2019-12-02 23:46:19 -0300

    add styled components

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+  height: 200px;
+  background: yellow;
+`
 
 const Search = () => {
-  return 'Search'
+  return <Section />
 }
 
 export default Search
--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+  height: 500px;
+  background: blue;
+`
 
 const Weather = () => {
-  return 'Weather'
+  return <Section />
 }
 
 export default Weather
