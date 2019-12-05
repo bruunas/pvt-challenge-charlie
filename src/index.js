d1c2b7eb43c8ee9a73938a5d59002968314026b2
Bruna Santos <brunas.dev@gmail.com>
2019-12-05 20:47:29 -0300

    update structure

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom"
-import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './style.css'
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 import getData from './getData'
 
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('root'));
 getData()
 serviceWorker.unregister();
