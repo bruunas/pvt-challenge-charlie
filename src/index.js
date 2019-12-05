3175686a0c76846f8a0375e9633f1dcbb476327b
Bruna Santos <brunas.dev@gmail.com>
2019-12-05 15:11:52 -0300

    add get component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom"
 import App from './app'
 import './style.css'
+import getData from './getData'
+
+getData()
 
 const container = document.getElementById("app")
 
